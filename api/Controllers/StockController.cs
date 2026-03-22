ce6196f3573bdaf89d5dfccd2b187bd73d965ec7
Ishan662 <ishanmadurangai662@gmail.com>
2026-03-22 19:29:55 +0530

    changed the api route from stock1 to stock

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
-    [Route("api/stock1")]
+    [Route("api/stock")]
     [ApiController]
     public class StockController : ControllerBase
     {
